c67c182c95065b2ae7d2442cdce65235c3130bb0
penghao1 <penghao1@staff.sina.com.cn>
2022-03-21 14:25:47 +0800

    测试位

--- objc4-838可编译源码/KCObjcBuild/main.m
+++ objc4-838可编译源码/KCObjcBuild/main.m
@@ -16,7 +16,39 @@
 
 #import <Foundation/Foundation.h>
 
+
+struct Student {
+    uintptr_t a:1;// 低位，放在低地址
+    uintptr_t b:1;//
+    uintptr_t c:1;//
+    uintptr_t d:1;//
+    uintptr_t e:1;//
+    uintptr_t f:1;//
+    uintptr_t g:1;//
+    uintptr_t h:1;//
+    uintptr_t i:8;//
+};
+
+void testBit(void) {
+    struct Student stu;
+    stu.a = 1;
+    stu.b = 1;
+    stu.c = 1;
+    stu.d = 1;
+    stu.e = 1;
+    stu.f = 0;
+    stu.g = 0;
+    stu.h = 1;
+    stu.i = 10;
+    
+    printf("%p",&stu);
+    //stu的值: 9f 0a
+}
+
+
 int main(int argc, const char * argv[]) {
+    testBit();
+
     @autoreleasepool {
         NSLog(@"Hello, KCObjcBuild!");
         NSObject *objc = [NSObject alloc];
@@ -24,3 +56,4 @@ int main(int argc, const char * argv[]) {
     }
     return 0;
 }
+
