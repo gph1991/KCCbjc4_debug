01d57a0d75ad01a16be705fdf153b11d4789433a
knuth <knuth0505@163.com>
2022-12-09 17:27:21 +0800

    引入Runloop

---
+++ swift-corelibs-foundation-main/debug/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  debug
+//
+//  Created by farmguo on 2021/3/26.
+//  Copyright © 2021 Swift. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    CFRunLoopRun();
+//    @autoreleasepool {
+        // insert code here...
+//        NSLog(@"Hello, World!");
+//    }
+    return 0;
+}
